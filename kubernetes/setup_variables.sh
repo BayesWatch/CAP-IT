491832963e9662aa042f23fef7eebd0be9631096
Antreas Antoniou <iam@antreas.io>
2023-01-08 06:22:17 +0000

    Push

--- kubernetes/setup_variables.sh
+++ kubernetes/setup_variables.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 export WANDB_API_KEY="03f97dc1ae484a463ef95ba2e03fb12f7eeddd85"
 export WANDB_ENTITY=machinelearningbrewery
-export WANDB_PROJECT=capit-v2-debug
+export WANDB_PROJECT=capit-v2
 
 export HF_USERNAME="evolvingfungus"
 export HF_TOKEN=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
 
 export TOKENIZERS_PARALLELISM=false
 
+export PROJECT_DIR=/app/
 export EXPERIMENTS_DIR=/run/experiments
 export EXPERIMENT_DIR=/run/experiments
 
@@ -18,10 +19,5 @@ export CLUSTER_NAME=spot-gpu-cluster-1
 export CLUSTER_ZONE=us-central1-a
 export CLUSTER_PROJECT=tali-multi-modal
 
-export EXPERIMENT_NAME_PREFIX="capit-v2-debug"
+export EXPERIMENT_NAME_PREFIX="capit-v2"
 export DOCKER_IMAGE_PATH="ghcr.io/bayeswatch/capit:latest"
-
-mkdir -p "$HOME/.huggingface"
-touch "$HOME/.huggingface/token"
-
-echo $HF_TOKEN >"$HOME/.huggingface/token"
