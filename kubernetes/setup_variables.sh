7c03b824e7f4a5678db36b253a3bf7666442a5d1
Antreas Antoniou <iam@antreas.io>
2023-02-18 18:15:01 +0000

    Updates

--- kubernetes/setup_variables.sh
+++ kubernetes/setup_variables.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export WANDB_API_KEY="821661c6ee1657a2717093701ab76574ae1a9be0"
 export WANDB_ENTITY=machinelearningbrewery
-export WANDB_PROJECT=capit-debug-4
+export WANDB_PROJECT=capit-debug-multi-gpu
 
-export HF_USERNAME="evolving-eukaryote"
-export HF_TOKEN=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
+export HF_USERNAME="Antreas"
+export HF_TOKEN=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
 
 export TOKENIZERS_PARALLELISM=true
 
@@ -20,4 +20,4 @@ export CLUSTER_ZONE=us-central1-a
 export CLUSTER_PROJECT=tali-multi-modal
 
 export EXPERIMENT_NAME_PREFIX="capit-v3"
-export DOCKER_IMAGE_PATH="ghcr.io/bayeswatch/capit:latest"
+export DOCKER_IMAGE_PATH="ghcr.io/antreasantoniou/capit:latest"
